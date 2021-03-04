3a533837b50a2a60ea9580f938e362344529adc6
xoxtomon <44625096+xoxtomon@users.noreply.github.com>
2021-03-04 10:50:13 -0500

    Create cola.h

---
+++ Actividad3/cola.h
@@ -0,0 +1,25 @@
+//=============================================================================
+// Sample Application: Lighting (Per Fragment Phong)
+//=============================================================================
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "glApplication.h"
+#include "glutWindow.h"
+#include <iostream>
+#include "glsl.h"
+#include <time.h>
+
+//-----------------------------------------------------------------------------
+
+#pragma once
+class cola
+{
+	public:
+		cola();
+
+		void DibujarCola();
+
+		~cola();
+};
+
